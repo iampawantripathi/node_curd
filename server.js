933d5fda9e79dd3a07b64639ca624a701427b239
iampawantripathi <tripathipawan1187@gmail.com>
2025-04-27 13:19:36 +0530

    changes on live

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
 const TodoRoute = require('./src/routes/route.js');
-=======
-const UserRoute = require('./src/routes/route.js');
->>>>>>> d0605782ab2ca2d3a4295d134cdfdf83d3a1c3a9
 const cors = require('cors');
 const { isCelebrateError } = require('celebrate');
 const dotenv = require('dotenv').config();
 
 const { REE, RES } = require('./src/services/service.js');
 
-<<<<<<< HEAD
 const connectDB  = require('./src/config/mongodb_con.js');
-=======
-require('./src/config/mongodb_con.js');
->>>>>>> d0605782ab2ca2d3a4295d134cdfdf83d3a1c3a9
 
 const app = express();
 
@@ -29,11 +21,7 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-<<<<<<< HEAD
 app.use('/', TodoRoute)
-=======
-app.use('/', UserRoute)
->>>>>>> d0605782ab2ca2d3a4295d134cdfdf83d3a1c3a9
 
 app.use((req, res) => {
     REE(res, 'Endpoint not found', 404);
@@ -60,18 +48,9 @@ app.get('/', (req, res) => {
     RES(res, 'Server is running successfully', 200);
 });
 
-<<<<<<< HEAD
-// app.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-// });
 connectDB()
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('MongoDB connection error:', err));
-=======
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
->>>>>>> d0605782ab2ca2d3a4295d134cdfdf83d3a1c3a9
--- src/config/mongodb_con.js
+++ src/config/mongodb_con.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config();
 
 const URL = dotenv.parsed.DB_URL;
 
-<<<<<<< HEAD
 const connectDB = () => {
     return mongoose.connect(URL, {
         useNewUrlParser: true,
@@ -12,14 +11,3 @@ const connectDB = () => {
 };
 
 module.exports = connectDB;
-=======
-mongoose.connect(URL, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Mongodb connection has been established successfully!");
-}).catch(err => {
-    console.log('Could not connect to the database', err);
-    process.exit();
-});
-
->>>>>>> d0605782ab2ca2d3a4295d134cdfdf83d3a1c3a9
